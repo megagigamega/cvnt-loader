7ba64d72048c44f83a082e2c96ef3c29c31c267b
megagigamega <ila132792@gmail.com>
2026-01-04 15:23:47 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,6 @@
 -- CVNT Loader
 
-if getgenv().CVNT_LOADED then
-    return
-end
+if getgenv().CVNT_LOADED then return end
 getgenv().CVNT_LOADED = true
 
 local HttpService = game:GetService("HttpService")
@@ -12,29 +10,43 @@ if not getgenv().Key then
     error("Key not found")
 end
 
+-- универсальный request
+local requestFunc =
+    request or
+    http_request or
+    (syn and syn.request)
+
+if not requestFunc then
+    error("HTTP request not supported")
+end
+
 local function getHWID()
     local exec = identifyexecutor and identifyexecutor() or "unknown"
     return tostring(Players.LocalPlayer.UserId) .. ":" .. exec
 end
 
-local data = {
+local payload = {
     key = getgenv().Key,
     hwid = getHWID()
 }
 
-local response = request({
-    Url = "http://ТВОЙ_IP:8080/check_key",
+local response = requestFunc({
+    Url = "http://185.185.68.56:8080/check_key", -- <-- ТВОЙ IP
     Method = "POST",
     Headers = {
         ["Content-Type"] = "application/json"
     },
-    Body = HttpService:JSONEncode(data)
+    Body = HttpService:JSONEncode(payload)
 })
+
+if not response or not response.Body then
+    error("API not responding")
+end
 
 local result = HttpService:JSONDecode(response.Body)
 
 if result.status ~= "ok" then
-    error("Key invalid")
+    error("Key invalid or expired")
 end
 
 loadstring(game:HttpGet(
